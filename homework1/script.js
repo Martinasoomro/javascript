c33384be2aa78c5859de53c03a3cc8a50d13b5f7
Martinasoomro <martinasoomro@hotmail.com>
2020-04-03 20:07:24 +0200

    first commit

---
+++ homework1/script.js
@@ -0,0 +1,8 @@
+// let name = prompt('What is your name?');
+// alert(`Hello ${name}`);
+
+// let number1 = prompt('First number');
+// let number2 = prompt('Second number');
+
+// alert(number1 ** 2);
+// alert(number2 ** 2);
