2963060fc2afa252dcc3f48625b7786d306aab62
Martinasoomro <martinasoomro@hotmail.com>
2020-04-06 16:59:01 +0200

    finished homework1

--- homework1/script.js
+++ homework1/script.js
@@ -2,7 +2,7 @@
 // Request the length of a side of a square
 // Display the perimeter of a square in the console
 
-let sideOfSquare = prompt ('Type the length of one side of a square:');
+let sideOfSquare = prompt("Type the length of one side of a square:");
 let perimeter = sideOfSquare * 4;
 console.log(perimeter);
 
@@ -18,19 +18,18 @@ let yearOfBirth = prompt("When were you born?");
 let age = currentYear - yearOfBirth;
 console.log(age);
 
-
 // --------------------------------------------
 
 // Assignment 3
-// Request a distance in kilometers between the two cities from a user, 
+// Request a distance in kilometers between the two cities from a user,
 // and the time they want to cover it within
 // Count and display the speed needed to be on time
 
-let distance = prompt('How far is the distance in kilometres?');
-let time = prompt('How much time do you have?');
-let timeInMinutes = 60;
-let speed = distance / timeInMinutes;
-console.log(speed + 'km');
+let distance = prompt("How far is the distance in kilometres?");
+let timeInMinutes = prompt("How much time (in minutes) do you have?");
+
+let speed = distance / (timeInMinutes / 60);
+console.log(speed + " km/h");
 
 // ---------------------------------------------
 
@@ -38,9 +37,12 @@ console.log(speed + 'km');
 // Create a simple currency converter
 // A user types in USD and the program converts them to EUR
 
-let usd = prompt('How many dollars do you want to convert?');
-const exchangeRate = usd * 0.92;
-console.log(exchangeRate + 'EUR');
+const exchangeRate = 0.92;
+
+let usd = prompt("How many USD do you want to convert to EUR?");
+let eur = usd * exchangeRate;
+
+console.log(eur + " EUR");
 
 // -------------------------------------------------
 
@@ -49,24 +51,27 @@ console.log(exchangeRate + 'EUR');
 // The program has to calculate the x
 // Display the result in the console
 
-let a = prompt('Type in a');
-let b = prompt('Type in b');
-let x = b / a;
+let a = prompt("Type in a");
+let b = prompt("Type in b");
+// a * x = 0 - b => (a * x) / a = -b / a => x = -b/a
+let x = -b / a;
 console.log(x);
 
 // ------------------------------------------------
 
 // Assignment 6
-// Request current time from a user in format HH:mm 
+// Request current time from a user in format HH:mm
 // (HH is hour in a day from 00 to 23, mm are minutes in hour)
 // Display how many hours and minutes are left until tomorrow
 
-let currentTime = prompt('What time is it?');
-let hours = currentTime
-let minutes = currentTime
+let currentTime = prompt("What time is it (HH:mm)?");
 
+let hours = parseInt(currentTime.slice(0, 2));
+let minutes = parseInt(currentTime.slice(3, 5));
 
+let hoursUntilTomorrow = 24 - hours;
+let minutesUntilTomorrow = 60 - minutes;
 
+let timeUntilTomorrow = hoursUntilTomorrow + ":" + minutesUntilTomorrow;
 
-
-
+console.log(timeUntilTomorrow + " until tomorrow");
