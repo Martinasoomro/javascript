b24abd5d466516992b697def3e91ec8bf778085b
Martinasoomro <martinasoomro@hotmail.com>
2020-04-27 19:40:06 +0200

    Classwork lesson 31 and 32

---
+++ homework7/script.js
@@ -0,0 +1,32 @@
+//CLASSWORK LESSON 31 AND 32
+
+// 1.Create an HTML page that will contain text and a button.
+// Clicking the button should hide the text. Clicking it again
+// should display the text.
+
+// function hideOrDisplay() {
+//   let x = document.getElementById(“myDIV”);
+//   if (x.style.display === “none”) {
+//     x.style.display = “block”;
+//   } else {
+//     x.style.display = “none”;
+//   }
+// }
+//----------------------------------------------------------
+
+// 2.Create an HTML page with a progress bar and a button. Clicking
+// the button should fill the progress bar by 5%. To create a progress
+// bar you can use <progress> tag
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/progress
+
+// function load() {
+//    let file = document.getElementById('file');
+//    let value = +file.getAttribute('value');
+//    file.setAttribute('value', value + 5);
+// }
+//---------------------------------------------------------
+
+// 3.Create an HTML page with a Like button. Clicking it should increase
+// the number in the likes counter.
+
+//---------------------------------------------------------
