6356190943a7965ed98e683c811f79584ba6c122
Martinasoomro <martinasoomro@hotmail.com>
2020-05-16 13:45:05 +0200

    finished homework

---
+++ homework4/script.js
@@ -0,0 +1,36 @@
+//1. Write a program that lists the properties of an object. For example, for the object
+
+let student = {
+  name: "Jane Rayy",
+  class: "VI",
+  age: 12,
+};
+
+//Output will be: name, class, age
+
+// 2.  Create an object that holds information of your favorite recipe.
+// It should have properties for title (a string), servings (a number),
+// and ingredients (a string, each ingredient separated by a comma).
+// Display the recipe information on the webpage. Use heading for the title.
+// Use paragraphs for other info. For example:
+
+Mole;
+Servings: 2;
+Ingredients: cumin, cocoa, cinnamon;
+
+Write a function to get a copy of the object where the keys have become the 
+values and the values become the keys. For example, for the object
+
+let person = {
+    name: 'Alex',
+    lastname: 'Doe',
+    age: 25,
+  };
+  The resulting object will be
+  {
+     Alex: 'name',
+     Doe: 'lastname',
+     '25': 'age'
+  };
+
+//----------------------------------------------------------------------
